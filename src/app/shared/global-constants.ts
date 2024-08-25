592009ba45b139b88ca9f86171daa1ed6623894a
Simon Lai <f88083@gmail.com>
2024-08-25 17:17:06 +0800

    fix: change 'DD' to 'dd' to follow the rule of date pipe

--- src/app/shared/global-constants.ts
+++ src/app/shared/global-constants.ts
@@ -1,5 +1,5 @@
 export class GlobalConstants {
     constructor() {}
 
-    public static readonly DATE_TIME_FORMAT = 'YYYY-MM-DDTHH:mm:ss';
+    public static readonly DATE_TIME_FORMAT = 'YYYY-MM-ddTHH:mm:ss';
 }
