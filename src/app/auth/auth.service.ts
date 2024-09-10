eae271efa96bf652ea0315eb54fff28f50c5c8e5
Simon Lai <f88083@gmail.com>
2024-09-10 15:14:05 +0800

    feat: request with the jwt token from the backend

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,14 +9,18 @@ import { tap } from 'rxjs/operators';
 export class AuthService {
   private apiUrl = 'http://localhost:8080/api/v1/auth';
   private isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
+  private token: string | null = null;
 
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string): Observable<any> {
-    return this.http.post<{ message: string; username?: string }>(`${this.apiUrl}/login`, { username, password }).pipe(
+    return this.http.post<{ token: string; username: string; message: string }>(`${this.apiUrl}/login`, { username, password }).pipe(
       tap((response) => {
-        if (response.username) {
+        if (response.token) {
+          this.token = response.token;
           this.isAuthenticatedSubject.next(true);
+          // TODO: Use cookie
+          localStorage.setItem('token', response.token);
         }
       })
     );
@@ -24,6 +28,12 @@ export class AuthService {
 
   logout(): void {
     this.isAuthenticatedSubject.next(false);
+    this.token = null;
+    localStorage.removeItem('token');
+  }
+
+  getToken(): string | null {
+    return this.token || localStorage.getItem('token');
   }
 
   isAuthenticated(): Observable<boolean> {
