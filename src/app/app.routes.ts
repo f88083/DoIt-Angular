d015a0ade2458d47263863d973c34ae1a922621d
Simon Lai <f88083@gmail.com>
2024-10-02 15:21:42 +0800

    style: remove auth.guard since abandoned

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login.component';
 import { TaskListComponent } from './task.component';
-import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
